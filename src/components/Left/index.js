bb400b16ed6f5759ba6ede837502042dac541f29
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-08 15:03:11 +0300

    Second commit All most done with the left and right sections remaining with only the middle one

--- src/components/Left/index.js
+++ src/components/Left/index.js
@@ -1,21 +1,77 @@
 import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PersonIcon from "@material-ui/icons/Person";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import ExploreIcon from "@material-ui/icons/Explore";
+import LockIcon from "@material-ui/icons/Lock";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 
 import "./left.css";
 
 export default function Left() {
   return (
     <div className="left__container">
-      <ul>
-        <li>Twitter</li>
-        <li>Explore</li>
-        <li>Notifications</li>
-        <li>Messages</li>
-        <li>Bookmarks</li>
-        <li>Lists</li>
-        <li>Profile</li>
-        <li>More</li>
+      <TwitterIcon className="list__icon" />
+      <ul className="left__text">
+        <li>
+          <HomeIcon />
+          <span>Home</span>
+        </li>
+        <li>
+          <ExploreIcon /> <span>Explore</span>
+        </li>
+        <li>
+          <NotificationsIcon />
+          <span>Notifications</span>
+        </li>
+        <li>
+          <MailOutlineIcon />
+          <span>Messages</span>
+        </li>
+        <li>
+          <BookmarkBorderIcon />
+          <span>Bookmarks</span>
+        </li>
+        <li>
+          <ListAltIcon />
+          <span>Lists</span>
+        </li>
+        <li>
+          <PersonIcon />
+          <span>Profile</span>
+        </li>
+        <li>
+          <MoreHorizIcon />
+          <span> More</span>
+        </li>
       </ul>
-      <button>Tweet</button>
+      <button className="button__tweet">Tweet</button>
+
+      <div className="profile__down">
+        <img
+          alt=""
+          src="https://pbs.twimg.com/profile_images/1285140955922931712/EzqruMt8_normal.jpg"
+        />
+        <div className="profile__names">
+          <p>
+            {" "}
+            Sseonga e..{" "}
+            <span>
+              {" "}
+              <LockIcon />
+            </span>
+          </p>
+          <p>@senoga4real</p>
+        </div>
+        <div>
+          <KeyboardArrowDownIcon />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Right/index.js
+++ src/components/Right/index.js
@@ -1,11 +1,109 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import "./right.css";
 
 export default function Right() {
   return (
-    <div>
-      <h1>Right</h1>
+    <div className="right__container">
+      <div className="input__wrapper">
+        <input type="search" placeholder="Search twitter" />
+        <span>
+          <SearchIcon />
+        </span>
+      </div>
+
+      {/* whats happeinf */}
+      <div className="right__happening">
+        <div className="happening__title">
+          <h3>Trends For You</h3>
+          <SettingsIcon />
+        </div>
+        <div className="card">
+          <header>
+            <p>Trending in Uganda</p>
+            <ExpandMoreIcon />
+          </header>
+          <article>
+            <h5>Bobi Wine </h5>
+            <p>245k Tweets</p>
+          </article>
+        </div>
+
+        <div className="card">
+          <header>
+            <p>Trending in Uganda</p>
+            <ExpandMoreIcon />
+          </header>
+          <article>
+            <h5>Bobi Wine </h5>
+            <p>245k Tweets</p>
+          </article>
+        </div>
+        <div className="card">
+          <header>
+            <p>Trending in Uganda</p>
+            <ExpandMoreIcon />
+          </header>
+          <article>
+            <h5>Bobi Wine </h5>
+            <p>245k Tweets</p>
+          </article>
+        </div>
+        <div className="card">
+          <header>
+            <p>Trending in Uganda</p>
+            <ExpandMoreIcon />
+          </header>
+          <article>
+            <h5>Bobi Wine </h5>
+            <p>245k Tweets</p>
+          </article>
+        </div>
+        <div className="card">
+          <header>
+            <p>Trending in Uganda</p>
+            <ExpandMoreIcon />
+          </header>
+          <article>
+            <h5>Bobi Wine </h5>
+            <p>245k Tweets</p>
+          </article>
+        </div>
+        <div className="showmore">
+          <h3>Show more </h3>
+        </div>
+      </div>
+      {/* who to follow */}
+      <div className="card__follow">
+        <header>
+          <h3>Who to follow</h3>
+        </header>
+        <div className="user">
+          <img
+            alt=""
+            src="https://pbs.twimg.com/profile_images/1222136283339853833/dr25E9Lk_bigger.jpg"
+          />
+          <div className="user__text">
+            <h5>Danny Thampson</h5>
+            <p>@DThompsonDev</p>
+          </div>
+          <button className="follow_btn">Follow</button>
+        </div>
+        <div className="user">
+          <img
+            alt=""
+            src="https://pbs.twimg.com/profile_images/1222136283339853833/dr25E9Lk_bigger.jpg"
+          />
+          <div className="user__text">
+            <h5>Danny Thampson</h5>
+            <p>@DThompsonDev</p>
+          </div>
+          <button className="follow_btn">Follow</button>
+        </div>
+      </div>
     </div>
   );
 }
